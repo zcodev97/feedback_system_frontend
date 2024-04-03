aad9114118ccdfcc88ad50b1c4438e1de79a5347
zcodev97 <zcodev97@gmail.com>
2024-04-04 01:06:30 +0300

    new build

--- src/global.js
+++ src/global.js
@@ -10,8 +10,8 @@ function formatDate(date = new Date()) {
   return [year, month, day].join("-");
 }
 
-// const SYSTEM_URL = "http://38.180.105.203:8010/";
-const SYSTEM_URL = "http://localhost:8000/";
+const SYSTEM_URL = "http://38.180.105.203:8099/";
+// const SYSTEM_URL = "http://localhost:8000/";
 // const SYSTEM_URL = "http://18.158.82.59:8010/";
 
 export { formatDate, SYSTEM_URL };
